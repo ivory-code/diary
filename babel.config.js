3eb4a72cc4503971ccb76dc288bd1f3bee1c72b8
ivory-code <kyo3553@gmail.com>
2024-05-26 19:31:55 +0900

    feat: add initial setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,45 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        safe: true,
+      },
+    ],
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@assets': './src/assets',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@navigations': './src/navigations',
+          '@utils': './src/utils',
+          src: './src',
+          '@types': './src/types',
+          '@epics': './epics',
+        },
+      },
+    ],
+    [
+      'dotenv-import',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: false,
+      },
+    ],
+    ['@babel/plugin-transform-private-methods', {loose: true}],
+  ],
+  env: {
+    production: {
+      plugins: ['transform-remove-console', 'react-native-paper/babel'],
+    },
+  },
 };
